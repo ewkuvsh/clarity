8a5b975f84c5bcbf4176d587d3ac2a2621be53ed
Evan Kuvshinoff <evan@kuvsinoff.net>
2024-11-18 18:31:57 -0500

    updated

--- clarity.py
+++ clarity.py
@@ -3,92 +3,73 @@ import os
 import sys
 import json
 import serial
-import search  #local file search.py
-from datetime import date
 from dotenv import load_dotenv
+from datetime import date
 
+# Constants and setup
 
-USB_PORT = "/dev/ttyACM0"
-try:
-        usb = serial.Serial(USB_PORT,9600,timeout = 2)
-except:
-        print("couldn't find the arduino, exiting...")
-        exit()
-
-
-        
 load_dotenv()
 client = OpenAI()
 args = sys.argv
 GPT_MODEL = "gpt-4o-mini"
 
 
-
+# Define functions
 def get_secret_code():
-    
-        return 'twiddlevee!'
+    return "twiddlevee!"
 
 def turn():
-        usb.write(b'turn')
-        return "turned"
+    USB_PORT = "/dev/ttyACM0"
+    try:
+        usb = serial.Serial(USB_PORT, 9600, timeout=2)
+    except:
+        print("Couldn't find the Arduino, exiting...")
+        exit()
+    
+    usb.write(b'turn')
+    return "turned"
 
-        
-print("\n")
+# Prepare messages and tools
+del args[0]
+today_date = date.today()
 
-
+prompt = ''.join(str(arg) + " " for arg in args)
+messages = [
+    {"role": "system", "content": "You are a robot."},
+    {"role": "user", "content": prompt},
+]
 
 tools = [
-
     {
         "type": "function",
         "function": {
             "name": "get_secret_code",
             "description": "Gets the secret code",
-           
         },
     },
-
     {
-      "type": "function",
-      "function": {
-        "name": "turn",
-        "description": "turns the servo 180 degrees and then back",
-       
-      }
-    }
-
-
-
-  
-
-            
-
-
+        "type": "function",
+        "function": {
+            "name": "turn",
+            "description": "Turns the servo 180 degrees and then back.",
+        },
+    },
 ]
 
-
-del(args[0])
-date = date.today()
-
-
-prompt = ''.join(str(arg)+" " for arg in args)
-
-messages=[
-    {"role": "system", "content": "You are a robot." },
-    {"role": "user", "content": prompt}
-  ]
-
+# Generate completion
 completion = client.chat.completions.create(
-    model = GPT_MODEL,
-    messages = messages,
-    tools = tools)
+    model=GPT_MODEL,
+    messages=messages,
+    tools=tools
+)
 
 response_message = completion.choices[0].message
 messages.append(response_message)
 
+# Debug: Check the response message
+#print("Response Message:", response_message)
 
-
-
+# Handle tool calls
 
 tool_calls = response_message.tool_calls
 if tool_calls:
@@ -107,7 +88,9 @@ if tool_calls:
         model_response_with_function_call = client.chat.completions.create(
             model=GPT_MODEL,
             messages=messages,
-        ) 
+        )
+        print(model_response_with_function_call.choices[0].message.content)
+
 
 
     elif tool_function_name == 'turn':
